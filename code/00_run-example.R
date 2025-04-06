7f5d816c13d8919b3f90cba4e618756435cca401
vanichols <virginia.nichols@gmail.com>
2025-04-06 21:56:46 +0200

    made good example using new pkgs

--- code/00_run-example.R
+++ code/00_run-example.R
@@ -2,49 +2,100 @@
 
 library(tidyverse)
 library(readxl)
+library(patchwork)
 library(PESTO)
 library(PesticideLoadIndex4Dummies) #--need to test this more...
-
-
 
 # A. qualitative data -----------------------------------------------------
 
 a <- read_excel("data/elicitations/ABC_survey_fairy dust lettuce.xlsx")
 
-a1 <- MakePestoDF(f_dat = a)
-a2 <- SummarizePestoDF(f_dat2 = a1)
-a3 <- CalcPestoUtil(f_dat3 = a2)
+d_6cat <- MakePestoDF(f_dat = a)
+d_all <- SummarizePestoDF(f_dat2 = d_6cat)
+d_util <- CalcPestoUtil(f_dat3 = d_all)
 
 # B. pesticide data data -----------------------------------------------------
 
-b <- read_excel("data/elicitations/ABC_peticides_fairy dust lettuce.xlsx")
+b1 <- read_excel("data/elicitations/ABC_peticides_fairy dust lettuce.xlsx")
+
+b2 <- 
+  b1 %>% 
+  mutate_at(c("ai_conc_in_prod", "amt_prod_app"), as.numeric) %>% 
+  fill(title) %>% 
+  fill(package_title)
 
 #--it is my job to fix the units
+b3 <- 
+  b2 %>% 
+  mutate(ai_conc_in_prod2 = 
+           case_when(
+             ai_conc_units == "g/100g" ~ ai_conc_in_prod * 10,
+             ai_conc_units == "g/kg" ~ ai_conc_in_prod,
+             ai_conc_units == "g/L" ~ ai_conc_in_prod,
+             TRUE ~ 999))
+
+#--amount ai applied
+b4 <- 
+  b3 %>% 
+  mutate(ai_applied = ai_conc_in_prod2 * amt_prod_app) %>% 
+  select(title, package_title, ai_name, ai_applied)
+  
+#--get plis
+b5 <- 
+  b4 %>% 
+  left_join(pli_ppdb3plis %>% rename(ai_name = substance)) %>% 
+  select(-id) %>% 
+  replace(is.na(.), 0)
+
+#--mean of the three components?
+b6 <- 
+  b5 %>% 
+  select(title, package_title, ai_name, ai_applied, 
+         env_load2, hh_load2, eco_load2) %>% 
+  pivot_longer(env_load2:eco_load2) %>% 
+  group_by(title, package_title, ai_name, ai_applied) %>% 
+  summarise(mean_pli = mean(value))
+
+#--multiply by app rate
+b7 <- 
+  b6 %>% 
+  mutate(pli_ha = round(mean_pli * ai_applied))
+
+#--sum by package
+d_pli <- 
+  b7 %>% 
+  group_by(title, package_title) %>% 
+  summarise(pli_ha = sum(pli_ha))
 
 
+# visualize ---------------------------------------------------------------
 
-dat.part.c <-
-  dat.part.a %>%
-  dplyr::left_join(internal_impactcatsinfo) %>%
-  dplyr::bind_rows(dat.part.b %>%
-                     dplyr::mutate(weight = 100)) %>%
-  dplyr::mutate(desc = paste0(value_metric, " (", weight, "%)"))
-
-t.title <- a_dat1$title %>% unique() %>% stringr::str_to_sentence()
+#--all six cats plot data
+p1 <- 
+  d_6cat %>%
+  dplyr::left_join(pesto_assessmentkey) %>%
+  dplyr::mutate(desc = paste0(performance_metric, " (", weight, "%)"))
 
 
-p1 <-
-  dat.part.c %>%
-  dplyr::filter(short != "all") %>%
-  dplyr::arrange(scenario, -weight) %>%
-  dplyr::mutate(value_binF = as.factor(value_bin),
-                value_metric = stringr::str_to_sentence(value_metric),
+p2 <- 
+  p1 %>% 
+  dplyr::arrange(package_title, -weight) %>%
+  dplyr::mutate(rating_textF = as.factor(rating_text),
+                value_binF = as.factor(value_bin),
                 descF = forcats::fct_inorder(desc),
-                scenarioF = factor(scenario, levels = c("Package #1",
-                                                        "Package #2"))) %>%
+                package_title = str_to_sentence(package_title))
+
+t.title <- p2$title %>% unique()
+
+f1 <- 
+  p2 %>%
   ggplot(aes(value_bin, score)) +
   geom_col(aes(fill = value_binF), linewidth = 1, color = "black", show.legend = F) +
-  scale_fill_manual(values = c(av1, av2, av3, av4, av5)) +
+  scale_fill_manual(values = c(pesto_pal[1],
+                               pesto_pal[2],
+                               pesto_pal[3],
+                               pesto_pal[4],
+                               pesto_pal[5])) +
   scale_y_continuous(limits = c(0, 100),
                      breaks = c(0, 25, 50, 75, 100),
                      position = "right") +
@@ -53,47 +104,42 @@ p1 <-
     labels = c("Very low", "Low", "Medium", "High", "Very high")) +
   labs(y = NULL,
        x = "Performance") +
-  th1 +
-  facet_grid(descF ~ scenarioF, labeller = label_wrap_gen(width = 20), switch = "y")
+  pesto_th1 +
+  theme(strip.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size = rel(1))) +
+  facet_grid(descF ~ package_title, 
+             labeller = label_wrap_gen(width = 20), switch = "y")
 
 
+p3 <- 
+  d_all %>% 
+  left_join(d_util) %>% 
+  left_join(d_pli)
 
-dat.part.c.util <-
-  dat.part.c %>%
-  dplyr::filter(short == "all") %>%
-  dplyr::arrange(scenario, -weight) %>%
-  dplyr::select(scenario, score, value_bin) %>%
-  dplyr::mutate(value = dplyr::case_when(
-    value_bin == 5 ~ 100,
-    value_bin == 4 ~ 75,
-    value_bin == 3 ~ 50,
-    value_bin == 2 ~ 25,
-    value_bin == 1 ~ 0,
-    TRUE ~ 9999
-  )) %>%
-  dplyr::group_by(scenario) %>%
-  dplyr::summarise(util = round(weighted.mean(x = value, w = score), 0)) %>%
-  dplyr::mutate(scenarioF = factor(scenario, levels = c("Package #1",
-                                                        "Package #2")),
-                util_label = paste0("Utility = ", util, "%"))
-
-p2 <-
-  dat.part.c %>%
-  dplyr::filter(short == "all") %>%
+p4 <-
+  p3 %>%
   dplyr::mutate(desc = "Weighted combination of all categories") %>%
-  dplyr::arrange(scenario, -weight) %>%
   dplyr::mutate(value_binF = as.factor(value_bin),
-                value_metric = stringr::str_to_sentence(value_metric),
                 descF = forcats::fct_inorder(desc),
-                scenarioF = factor(scenario, levels = c("Package #1",
-                                                        "Package #2"))) %>%
+                package_title = str_to_sentence(package_title),
+                util_label = paste0("Utility = ", util, "%"),
+                pli_label = paste0("Pesticide load = ", pli_ha))
+
+f2 <- 
+  p4 %>% 
   ggplot(aes(value_bin, score)) +
   geom_col(aes(fill = value_binF), linewidth = 1, color = "black", show.legend = F) +
-  geom_text(data = dat.part.c.util,
-            aes(x = 3, y = 90, label = util_label,
+  geom_text(aes(x = 3, y = 95, label = util_label,
                 fontface = "italic"),
-            hjust = 0.5) +
-  scale_fill_manual(values = c(av1, av2, av3, av4, av5)) +
+            hjust = 0.5, check_overlap = T) +
+  geom_text(aes(x = 3, y = 90, label = pli_label,
+                fontface = "italic"),
+            hjust = 0.5, check_overlap = T) +
+  scale_fill_manual(values = c(pesto_pal[1],
+                               pesto_pal[2],
+                               pesto_pal[3],
+                               pesto_pal[4],
+                               pesto_pal[5])) +
   scale_y_continuous(limits = c(0, 100),
                      breaks = c(0, 25, 50, 75, 100),
                      position = "right") +
@@ -102,30 +148,20 @@ p2 <-
     labels = c("Very low", "Low", "Medium", "High", "Very high")) +
   labs(y = NULL,
        x = "Performance") +
-  th1 +
-  facet_grid(descF ~ scenarioF, labeller = label_wrap_gen(width = 20), switch = "y")
+  pesto_th1 +
+  theme(strip.text.y = element_text(size = rel(1.5))) +
+  facet_grid(descF ~ package_title, labeller = label_wrap_gen(width = 20), switch = "y")
 
+f2
 
-layout <- "
-AABB
-AA##
-AA##"
+f1 + f2 + 
+  plot_layout(widths = c(1, 2)) +
+  plot_annotation(
+  title = "Denmark, lettuce",
+  subtitle = "Comparing the use of frog warts, fairy dust, and troll manure for weed control",
+  theme = theme(plot.title = element_text(size = rel(2)),
+                plot.subtitle = element_text(size = rel(1.5)))
+)
 
-theme_border <-
-  ggplot2::theme_gray() +
-  ggplot2::theme(plot.background = element_rect(fill = NA, colour = 'black', linewidth = 3),
-                 plot.title = element_text(size = 20, hjust = 0.5))
+ggsave("figs/fig_example-fairy-dust.png", width = 12, height = 8)
 
-t.title <- a_dat1$title %>% unique()
-
-
-p_all <-
-  p1 +
-  p2 +
-  patchwork::plot_layout(design = layout) +
-  patchwork::plot_annotation(title =   stringr::str_wrap(t.title, 40),
-                             theme = theme_border)
-
-
-
-ggsave("figs/fig_example-fairy-dust.png", width = 9, height = 9)
